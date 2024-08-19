cd5c4e603da41d249bee0e0076d2d5805066d689
Christian Bos <131701155+christian-bos-graafschapcollege@users.noreply.github.com>
2024-08-19 10:25:28 +0200

    Refactoring ingredients.js
    Na refactoring gebruikt dit script nu nette, leesbare functies

--- ingredients.js
+++ ingredients.js
@@ -1,13 +1,26 @@
-$(document).ready(function () {
+$(document).ready(() => {
     const params = (new URL(document.location)).searchParams;
-    const id = params.get('id');
+    const id = params.get('id'); 
+
+    const baseURL = 'https://api.spoonacular.com/recipes';
+    const apiKey = 'c25e2d349ba842ee8186ded1ff30b942';
 
     $.ajax({
-        url: `https://api.spoonacular.com/recipes/${id}/information?apiKey=c25e2d349ba842ee8186ded1ff30b942`
-    }).done(function (data) {
-        $('.recipe').append(`<h3 class="recipe-name">${data.title}</h3><img src="${data.image}"></img>`);
-        data.extendedIngredients.forEach(ingredient => {
-            $('.ingredients').append(`<div class="ingredient"><h3>${ingredient.name}</h3><img src="https://spoonacular.com/cdn/ingredients_100x100/${ingredient.image}"></img><p>${ingredient.amount} ${ingredient.unit}</p></div>`);
-        });
-    });
+        url: `${baseURL}/${id}/information?apiKey=${apiKey}`
+    }).done(handleResponse);
 });
+
+function handleResponse(data) {
+    // list recipe name and photo`
+    $('.recipe').append(`<h3 class="recipe-name">${data.title}</h3><img src="${data.image}"></img>`);
+
+    // loop over ingredients
+    data.extendedIngredients.forEach(ingredient => {
+        $('.ingredients').append(`
+            <div class="ingredient">
+                <h3>${ingredient.name}</h3>
+                <img src="https://spoonacular.com/cdn/ingredients_100x100/${ingredient.image}"></img>
+                <p>${ingredient.amount} ${ingredient.unit}</p>
+            </div>`);
+    })
+};
